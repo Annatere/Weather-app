f9ba258ca3931135c6c8d4ae57d298452cf5ba27
Annatere <90653410+Annatere@users.noreply.github.com>
2021-09-28 14:55:44 +0100

    upload files

---
+++ src/index.js
@@ -0,0 +1,116 @@
+let now = new Date();
+
+let fecha = document.querySelector("#fecha");
+
+let hours = now.getHours();
+let todayHour = document.querySelector("#hours");
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+todayHour.innerHTML = `${hours}`;
+
+let minutes = now.getMinutes();
+let todayMinutes = document.querySelector("#minutes");
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+todayMinutes.innerHTML = `${minutes}`;
+
+let dias = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let dia = dias[now.getDay()];
+
+fecha.innerHTML = `${dia} ${hours}:${minutes}`;
+
+////
+
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahTemperature = (17 * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahTemperature);
+}
+
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  let celsiusTemperature = 17;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+///
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#cid");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let maxElement = document.querySelector("#max");
+  maxElement.innerHTML = Math.round(response.data.main.temp_max);
+
+  let minElement = document.querySelector("#min");
+  minElement.innerHTML = Math.round(response.data.main.temp_min);
+
+  let feelElement = document.querySelector("#feel");
+  feelElement.innerHTML = Math.round(response.data.main.feels_like);
+}
+////
+function search(city) {
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#search-text");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#barraProcura");
+form.addEventListener("submit", handleSubmit);
+
+///current location///
+
+function GetCurrentLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(GetCurrentLocation);
+}
+
+let currentLocationButton = document.querySelector("#currentButton");
+currentLocationButton.addEventListener("click", getPosition);
