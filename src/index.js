e0f4941743362a5814151cd0359eae02241ad9a5
Annatere <90653410+Annatere@users.noreply.github.com>
2021-10-02 14:58:23 +0100

    Display the forecast data using  the API

--- src/index.js
+++ src/index.js
@@ -57,6 +57,15 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 ///
+function getForecast(coordinates){
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+
+
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -87,6 +96,8 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 
 }
 ////
@@ -123,31 +134,48 @@ function getPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(GetCurrentLocation);
 }
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
-function displayForecast(){
+  return days[day];
+
+
+
+
+
+}
+
+
+function displayForecast(response){
+
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   
   let forecastHTML = `<div class="row">`; 
-  let days = ["Thu","Fri","Sat","Sun"];
-  days.forEach(function(day){
-    forecastHTML = forecastHTML + `
+  forecast.forEach(function(forecastDay,index){
+    if (index < 6 ) {
+    forecastHTML = 
+     forecastHTML + 
+     `
           <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
             <img 
-              src="https://openweathermap.org/img/wn/50d@2x.png"
+              src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
               alt=""
               width="42"
             />
             <div class="weather-forecast-temperature">
               <span class="weather-forecast-temperature-max">
-              18°</span>
+              ${Math.round(forecastDay.temp.max)}°</span>
               <span class="weather-forecast-temperature-min">
-              12°</span>
+              ${Math.round(forecastDay.temp.min)}°</span>
             </div>
           </div>
     
-    `;
-
+     `;
+    }
   });
   
 
@@ -163,5 +191,5 @@ currentLocationButton.addEventListener("click", getPosition);
 
 search("Lisbon");
 
-displayForecast();
+
 
